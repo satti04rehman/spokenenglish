a55d4b3b2d3af139b4cfd4fedfd434cbb088ea2a
PMAS <pmas@example.com>
2026-04-10 19:22:13 +0500

    chore: remove leftover listUsers script

--- server/listUsers.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config({ path: '../.env' });
-const mongoose = require('mongoose');
-const User = require('./models/User');
-
-mongoose.connect(process.env.MONGODB_URI).then(async () => {
-  const users = await User.find().select('name studentId role');
-  console.log(JSON.stringify(users, null, 2));
-  process.exit(0);
-});
